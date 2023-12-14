8cba8deeed1f24ef22e80169a6b16b0995ee2f17
Fatima Alattas <AlattasFatima22@Gmail.com>
2023-12-14 23:58:57 +0300

    Update RandomQuotes.js

--- RandomQuotes.js
+++ RandomQuotes.js
@@ -9,16 +9,15 @@ const apikey="mxrLacJBnOMn+MzdYUOSJw==VViR7IoJBxeRoYG3";
 const options = {
     method:'GET',
     headers:{
-        "X-Api-Key": apikey, },
+        "X-Api-Key": apikey,
+    },
 };
 
-const apiURL = "https://api.api-ninjas.com/v1/quotes?category=happiness"
+const apiURL = "https://api.api-ninjas.com/v1/quotes"
 
 async function getQ(){
-    console.log('sd');
     const response = await fetch(apiURL, options);
     const data = await response.json();
-    console.log(data);
     Rquote.innerText = data[0].quote;
     person.innerText = data[0].author;
 };
