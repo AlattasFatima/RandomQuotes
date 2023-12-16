a2f134fead71bd8bde49f972683b4e40bf7e86be
Fatima Alattas <AlattasFatima22@Gmail.com>
2023-12-16 10:14:17 +0300

    Update RandomQuotes.js

--- RandomQuotes.js
+++ RandomQuotes.js
@@ -1,11 +1,12 @@
 // Varibales 
 let Rquote = document.querySelector('.quote');
 let person = document.querySelector('.person');
+const category = document.querySelector('#category');
 const btn =  document.getElementById('btn');
-
+const inputs =document.querySelector('input').value;
 
 const apikey="mxrLacJBnOMn+MzdYUOSJw==VViR7IoJBxeRoYG3";
-
+const apiURL = `https://api.api-ninjas.com/v1/quotes?category=${inputs}`;
 const options = {
     method:'GET',
     headers:{
@@ -13,13 +14,14 @@ const options = {
     },
 };
 
-const apiURL = "https://api.api-ninjas.com/v1/quotes"
-
 async function getQ(){
     const response = await fetch(apiURL, options);
     const data = await response.json();
     Rquote.innerText = data[0].quote;
     person.innerText = data[0].author;
+    category.innerText = data[0].category;
+    console.log(data);
+    console.log(inputs);
 };
 
 btn.addEventListener('click', getQ);
